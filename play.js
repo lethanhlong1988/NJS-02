908306f823dc31e260654467bacba40025c29af8
lethanhlong1988 <lethanhlong2008@gmail.com>
2024-05-23 16:09:39 +0900

    update

--- play.js
+++ play.js
@@ -35,10 +35,21 @@ const person = {
   },
 };
 
-const copiedPerson = { ...person };
-console.log(copiedPerson);
+const printName = ({ name }) => {
+  console.log(name);
+};
+
+printName(person);
+
+const { name, age } = person;
+console.log(name, age);
+
+// const copiedPerson = { ...person };
+// console.log(copiedPerson);
 
 const hobbies = ["Sports", "Cooking"];
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
 // for (let hobby of hobbies) {
 //   console.log(hobby);
 // }
@@ -49,10 +60,10 @@ const hobbies = ["Sports", "Cooking"];
 // console.log(hobbies);
 
 const copiedArray = [...hobbies];
-console.log(copiedArray);
+// console.log(copiedArray);
 
 const toArray = (...args) => {
   return args;
 };
 
-console.log(toArray(1, 2, 3, 4));
+// console.log(toArray(1, 2, 3, 4));
