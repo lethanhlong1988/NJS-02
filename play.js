94efc6be334f0c7caff36141659c4ea68f40243c
lethanhlong1988 <lethanhlong2008@gmail.com>
2024-05-23 15:52:44 +0900

    update

--- play.js
+++ play.js
@@ -1,6 +1,9 @@
-var name = "Long";
-var age = 36;
-var hasHobbies = true;
+/*
+const name = "Long";
+let age = 36;
+const hasHobbies = true;
+
+age = 50;
 
 function summarizeUser(userName, userAge, userHasHobby) {
   return (
@@ -13,4 +16,43 @@ function summarizeUser(userName, userAge, userHasHobby) {
   );
 }
 
+const add = (a, b) => a + b;
+const addOne = (a) => a + 1;
+const addRandom = () => 1 + 2;
+
+console.log(add(1, 2));
+console.log(addOne(1));
+console.log(addRandom());
+
 console.log(summarizeUser(name, age, hasHobbies));
+*/
+
+const person = {
+  name: "Long",
+  age: 36,
+  greet() {
+    console.log("Hi, I am " + this.name);
+  },
+};
+
+const copiedPerson = { ...person };
+console.log(copiedPerson);
+
+const hobbies = ["Sports", "Cooking"];
+// for (let hobby of hobbies) {
+//   console.log(hobby);
+// }
+
+// console.log(hobbies.map((hobby) => "Hobby: " + hobby));
+
+// person.greet();
+// console.log(hobbies);
+
+const copiedArray = [...hobbies];
+console.log(copiedArray);
+
+const toArray = (...args) => {
+  return args;
+};
+
+console.log(toArray(1, 2, 3, 4));
