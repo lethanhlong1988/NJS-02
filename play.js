b80868cd2e2c2efe3f3bbf784badeda0d0d48980
lethanhlong1988 <lethanhlong2008@gmail.com>
2024-05-24 10:51:53 +0900

    update

--- play.js
+++ play.js
@@ -81,7 +81,15 @@ const fetchData = () => {
 // fetchData();
 
 setTimeout(() => {
-  console.log("Kiem tra set time!!!");
+  console.log("Timer is done!!!");
+  fetchData()
+    .then((text) => {
+      console.log(text);
+      return fetchData();
+    })
+    .then((text2) => {
+      console.log(text2);
+    });
 }, 2000);
 
 console.log("Hello!!!");
